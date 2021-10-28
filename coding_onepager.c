eda0f5233a672fc7d9ca16959d5b2c9e3834e515
Abylay Ospan <aospan@jokersys.com>
2021-10-28 18:40:46 +0000

    coding_onepager.c: power function added
    power is equivalent to pow in math.h
    
    Signed-off-by: Abylay Ospan <aospan@jokersys.com>

--- coding_onepager.c
+++ coding_onepager.c
@@ -22,6 +22,24 @@ void swap (int *s, int *d) {
   *d = tmp;
 }
 
+// from http://www.geeksforgeeks.org/write-a-c-program-to-calculate-powxn/
+float power(float x, int y)
+{
+  float temp;
+  if( y == 0)
+    return 1;
+  temp = power(x, y/2);       
+  if (y%2 == 0)
+    return temp*temp;
+  else
+  {
+    if(y > 0)
+      return x*temp*temp;
+    else
+      return (temp*temp)/x;
+  }
+}
+
 /*************/
 /*** QUEUE ***/
 /*************/
